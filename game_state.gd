55adcc634e64a647e62b177b242c157e8b070378
Darius Stonys <krotas@gmail.com>
2024-01-28 00:47:08 +0200

    Add player detection and game failure state

--- game_state.gd
+++ game_state.gd
@@ -1,5 +1,12 @@
 extends Node
 
-func handle_game_over(state):
+func _on_game_over_failure(failure_message):
+	handle_game_over("failure", failure_message)
+
+func _on_game_over_success():
+	handle_game_over("success")
+
+func handle_game_over(state, failure_message=null):
+	print(failure_message)
 	get_tree().paused = true
 	# TODO: call game_over_UI, show state (victory or loss), show buttons for restarting or quiting the game
