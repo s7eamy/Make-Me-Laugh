38cfdac3bd32688cc27dc74ee5ae943a86564c5b
s7eamy <52354209+s7eamy@users.noreply.github.com>
2024-01-28 00:28:13 +0200

    implement title screen and game over screen

---
+++ title_screen.gd
@@ -0,0 +1,8 @@
+extends CanvasLayer
+
+func _on_quit_button_button_down() -> void:
+	get_tree().quit(0)
+
+func _on_start_button_button_down() -> void:
+	get_parent().get_node("Stopwatch").time = 0.0
+	queue_free()
