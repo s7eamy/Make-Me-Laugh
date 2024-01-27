034e5a2e8844aec9705bbbd073db05d237762dbf
s7eamy <52354209+s7eamy@users.noreply.github.com>
2024-01-27 13:42:51 +0200

    implement animation change on movement direction change

--- pax.gd
+++ pax.gd
@@ -1,13 +1,20 @@
 extends Node2D
 
 @onready var animation = $AnimatedSprite2D
+@onready var path = get_parent()
+@onready var last_position = path.position
 
 func _on_attacked() -> void:
 	print("Participant " + name + " was attacked!")
 	queue_free()
-	
+
 func _process(delta):
 	if get_parent().name == "PathFollow2D":
 		animation.play("walk")
 	else:
 		animation.play("stand")
+	
+	# change animation direction based on movement direction
+	var direction = last_position - path.position
+	animation.flip_h = direction.x - 0.2 > 0
+	last_position = path.position
