ea3e55054f18706a9b084085371033e51f26799f
Darius Stonys <krotas@gmail.com>
2024-01-27 16:50:32 +0200

    Add attack handling
    Updated Pax collider to use layer 2

--- Player.gd
+++ Player.gd
@@ -1,11 +1,31 @@
-extends CharacterBody2D
+extends Node2D
 
-@export var speed = 400
+signal attack
+var is_attacking = false
+var target
+
+func _physics_process(_delta):
+	get_input()
+	resolve_target()
+	handle_attack()
 
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+	is_attacking = Input.is_action_just_pressed("attack")
 
-func _physics_process(delta):
-	get_input()
-	move_and_slide()
+func resolve_target():
+	target = null
+	var min_distance = INF
+	var bodies = %AttackRadius.get_overlapping_bodies()
+	
+	for body in bodies:
+		var distance = position.distance_to(body.global_position)
+		if distance < min_distance:
+			min_distance = distance
+			target = body
+
+func handle_attack():
+	if not is_attacking:
+		return
+	
+	if target && target.get_parent() && target.get_parent().has_method("_on_attacked"):
+		target.get_parent()._on_attacked()
