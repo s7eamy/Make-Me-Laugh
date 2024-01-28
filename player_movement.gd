0449e57e4cd021d81e195276d6a0d00538de5ce2
s7eamy <52354209+s7eamy@users.noreply.github.com>
2024-01-28 11:37:52 +0200

    implement main character animations

--- player_movement.gd
+++ player_movement.gd
@@ -1,11 +1,39 @@
 extends CharacterBody2D
 
 @export var speed = 400
+@onready var animator_node = $PlayerAnimation
+@onready var last_direction = "down"
 
 func _physics_process(_delta):
 	get_input()
 	move_and_slide()
 
+func get_direction():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	
+	if input_direction == Vector2.LEFT:
+		return "left"
+	elif input_direction == Vector2.RIGHT:
+		return "right"
+	
+	if input_direction == Vector2.UP:
+		return "up"
+	elif input_direction == Vector2.DOWN:
+		return "down"
+	
+	if input_direction.y > 0:
+		return "down"
+	elif input_direction.y < 0:
+		return "up"
+	else:
+		return ""
+
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
+	var direction = get_direction() if get_direction() != "" else last_direction
+	if velocity == Vector2.ZERO:
+		animator_node.play_animation("idle", direction)
+	else:
+		animator_node.play_animation("walk", direction)
+	last_direction = direction
