8035725fbabb228ff9cc6eb6ab9f01164bcc824f
Etusas <149682828+Etusas@users.noreply.github.com>
2024-01-28 14:26:40 +0200

    TRAILER ONLY

--- pax_animation_a.gd
+++ pax_animation_a.gd
@@ -1,8 +1,15 @@
 extends Node2D
 
-var animations = get_children()
+@onready var animations = get_children()
+@export var direction = "down"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for animation in animations:
-		animation.play()
+		animation.play("idle" + direction.capitalize())
+		
+func _process(delta):
+	var ani = "Idle" + direction.capitalize()
+	for animation in animations:
+		if animation.animation != ani:
+			animation.play(ani)
