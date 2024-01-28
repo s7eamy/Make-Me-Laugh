4b336bfd28745bf39f1ea64039539d21341cc54a
s7eamy <52354209+s7eamy@users.noreply.github.com>
2024-01-28 14:16:37 +0200

    implement lightning strike on hazard activation

--- hazard.gd
+++ hazard.gd
@@ -5,3 +5,12 @@ func _on_activated():
 	for body in bodies:
 		if body && body.get_parent() && body.get_parent().has_method("_on_attacked"):
 			body.get_parent()._on_attacked()
+	
+	for anim_node in $LightningAnimation.get_children():
+		anim_node.visible = true
+		anim_node.play()
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	for anim_node in $LightningAnimation.get_children():
+		anim_node.visible = false
